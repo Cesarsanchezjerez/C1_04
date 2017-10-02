f26cfb6c78b1b9af633c5c321515c7b84047dd43
PedroRamirezVerger <32263959+PedroRamirezVerger@users.noreply.github.com>
2017-10-02 13:56:47 +0200

    Update Terreno.java

--- Terreno.java
+++ Terreno.java
@@ -2,22 +2,22 @@ package hito1;
 
 public class Terreno {
 
-	private int Terreno [][]; 
+	private int terreno [][]; 
 	private int X;//posicion del tractor
 	private int Y;//posicion del tractor
 	private int K;//cantidad de arrena deseada por casilla
-	private int Max;//maxima arena en una casilla
+	private int max;//maxima arena en una casilla
 	private int C;//cordenada del terreno
 	private int F;//2cordenada del terreno
 	//private
 	
 	//constructor
 	public Terreno(int [][] Nterreno,int NX,int NY,int NK,int NMax,int NC,int NF) {
-		Terreno=Nterreno;
+		terreno=Nterreno;
 		X=NX;
 		Y=NY;
 		K=NK;
-		Max=NMax;
+		max=NMax;
 		C=NC;
 		F=NF;
 	}
