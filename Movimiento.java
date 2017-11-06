16adc3d228a80ccd608bdd345c28e040d4a51ca4
sergiorodriguez25 <32263950+sergiorodriguez25@users.noreply.github.com>
2017-11-06 11:37:50 +0100

    Update Movimiento.java

--- Movimiento.java
+++ Movimiento.java
@@ -67,4 +67,75 @@ public class Movimiento {
 		
 		return cadena;
 	}
+public ArrayList<int[]> todasV(int repartir){
+		
+		ArrayList<int[]>sol=new ArrayList<int[]>();
+		movimientosRecurV(repartir, 0, new int[4] , sol, repartir);
+		
+		return sol;
+	}
+	public void movimientosRecurV ( int arenaQueda, int etapa, int [] actual, ArrayList<int[]> soluciones, int repartir) {
+		
+		if (arenaQueda==0 || etapa >=4) {
+			if (arenaQueda==0){
+				int[] aux=new int[4];
+				if (contarArena(actual)==repartir) {
+					System.arraycopy( actual, 0, aux, 0, actual.length);
+					soluciones.add(aux);
+				}
+				
+			}
+		}else {
+			for (int a = arenaQueda; a >= 0 ; a--) {
+				if((arenaQueda-a)>=0) {
+					actual[etapa]=a;	
+					movimientosRecurV(arenaQueda-a, etapa+1, actual, soluciones, repartir);
+				}
+				
+			}		
+		}
+			
+	}
+	public int contarArena(int[] a) {
+		int suma=0;
+		for (int i = 0; i < a.length; i++) {
+			suma+=a[i];
+		}
+		return suma;
+	}
+	public int[] nuevaCoordenada(int x, int y, int cardinal) {
+		int [] c= new int[2];
+		switch (cardinal) {
+		case 0:
+			y-=1;
+			break;
+		case 1:
+			y+=1;
+			break;
+		case 2:
+			x+=1;
+			break;
+		case 3:
+			x-=1;
+			break;
+		}
+		c[0]=x;
+		c[1]=y;
+		return c;
+	}
+	
+	
+	public void aplicarMovimiento(Terreno terreno, Movimiento m) {
+		int [] coordenadas= m.getPosicion();
+		int[][] matriz=terreno.getTerreno();
+		terreno.setX(coordenadas[0]);
+		terreno.setY(coordenadas[1]);
+		for (int i = 0; i < 4; i++) {
+			coordenadas=m.getDesde();
+			coordenadas=nuevaCoordenada(coordenadas[0], coordenadas[1], i);
+			matriz[coordenadas[0]][coordenadas[1]]=m.getArena()[i];
+		}
+		terreno.setTerreno(matriz);
+	}
 }
+
