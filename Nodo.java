e73c3defb624f5bd7bd57afafabd74337c249762
PedroRamirezVerger <32263959+PedroRamirezVerger@users.noreply.github.com>
2017-11-08 20:05:34 +0100

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -9,6 +9,13 @@ public class Nodo {
 	private int costo;
 	private Accion accion;
 	private int valor;
+	public Nodo() {
+		this.padre = padre;
+		this.estado = estado;
+		this.costo = costo;
+		this.accion = accion;
+		this.valor = valor;
+	}
 	public Nodo(Nodo padre, Estado estado, int costo, Accion accion, int valor) {
 		super();
 		this.padre = padre;
@@ -47,9 +54,49 @@ public class Nodo {
 	public void setValor(int valor) {
 		this.valor = valor;
 	}
-	/*public ArrayList<Nodo> sucesores(){
+	public ArrayList<Nodo> sucesores(){
 		ArrayList<Accion> todas=accion.todasV(estado.getRepartir());
+		ArrayList<Nodo> sucesores=new ArrayList<Nodo>();
+		Nodo me=new Nodo();
+		for (Accion a : todas) {
+			if(!fueraLimites(a,estado)) { 
+				if(!echaDemasiado(a, estado)) {
+					Nodo n=new Nodo(me, a.aplicarMovimiento(estado, a), a.cantidadArena(a.getArena())+1, a, valor); //valor es un random
+					sucesores.add(n);
+				}
+				
+			}
+		}
+		return sucesores;
 	}
-	*/
-	
+	public boolean fueraLimites(Accion a, Estado e) {
+		boolean sale=false;
+		int[] nCoor= new int[2];
+		int[] movimiento=a.getArena();
+		for (int i = 0; i < movimiento.length; i++) {
+			if (movimiento[i]!=0) {
+				nCoor= a.nuevaCoordenada(e.getx(), e.gety(), i);
+				if(nCoor[0]-1<0 || nCoor[1]-1<0 || nCoor[0]+1>=e.getc() || nCoor[1]+1>=e.getf()) {
+					sale=true;
+				}
+			}
+		}
+		return sale;
+	}
+	public boolean echaDemasiado(Accion a, Estado e) {
+		boolean demasiado=false;
+		int[] nCoor= new int[2];
+		int[] movimiento=a.getArena();
+		int[][] terreno=e.getTerreno();
+		for (int i = 0; i < movimiento.length; i++) {
+			if (movimiento[i]!=0) {
+				nCoor= a.nuevaCoordenada(e.getx(), e.gety(), i);
+				if(terreno[nCoor[0]][nCoor[1]]+movimiento[i]> e.getmax()) {
+					demasiado=true;
+				}
+			}
+		}
+		
+		return demasiado;
+	}
 }
